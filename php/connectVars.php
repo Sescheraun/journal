987041d2670e44167974592d2503b2fd42e54628
Sescheraun <Sescheraun@gmail.com>
2018-11-17 13:34:44 -0600

    Creating the initial php files

---
+++ php/connectVars.php
@@ -0,0 +1,12 @@
+<?php
+    // Define database connection constants
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASSWORD', 'root');
+    define('DB_NAME', 'journal');
+
+//    $home_url = 'http://'
+//           . $_SERVER['HTTP_HOST']
+//            . dirname($_SERVER['PHP_SELF'])
+//            . '/index.php';
+?>
