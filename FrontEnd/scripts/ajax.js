94d69c5bc34d45242ebf38b7d0656cfda9590ce0
Sescheraun <Sescheraun@gmail.com>
2018-11-23 08:40:32 -0600

    adding the basic structure of the first ajax call.

--- FrontEnd/scripts/ajax.js
+++ FrontEnd/scripts/ajax.js
@@ -1,7 +1,32 @@
 (($) => { 
+    let crudURL = "localhost/journal/php/crud.php";
+    
     $(document).ready( function() {
-        
+        alert("in the function");
+        alert(crudURL);
+        // Load Categories
+        $.ajax({
+            "url": crudURL
+            , "method":"GET"
+
+            , "success":function(result) {
+                alert(result);
+            }
+            , "error":function() {
+                alert("error");
+            }
+            , "complete":function() {
+                alert("Call complete");
+            }
+        })
+
+        // load all entries
+       // $.AJAX
     })
+
+    
+
+
 
 
 
