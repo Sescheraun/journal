ae37b4562fd89bd6bfec863e6c678f45a5be482e
Sescheraun <Sescheraun@gmail.com>
2018-11-25 09:55:14 -0600

    adding constats to handle the table names in the database

--- php/connectVars.php
+++ php/connectVars.php
@@ -4,5 +4,7 @@
     define('DB_USER', 'root');
     define('DB_PASSWORD', 'root');
     define('DB_NAME', 'journal');
-    define('TABLE_NAME', 'journal')
+    define('JOURNAL_TABLE', 'journal');
+    define('SUBJECT_TABLE','subject');
+    define('USER_TABLE','user');
 ?>
