8e017ac2f2d8ca7bb5feec775ed931e67d16666c
Sescheraun <Sescheraun@gmail.com>
2018-11-25 12:34:33 -0600

    Stubbing out the methods to handle the various kinds of read actions

--- php/Journal.php
+++ php/Journal.php
@@ -1,7 +1,7 @@
 <?php 
     
 
-    class journal {
+    class Journal {
         private $conn;
         private $id;
         private $dateOfEntry;
@@ -30,37 +30,66 @@
         }
         
 
+
+
         /** 
-         * This is the get all method.
+         * This is the read all method.
          * 
          * @return full array of the undeleted journal entries.
          */
         
         public function readAll() {
+            $whereClause = " WHERE isDeleted = ? ";
+
+            $bindable = false;
+
+            return $this->read($whereClause, $bindable);
+        }
+
+        /**
+         * This will be the readById
+         * 
+         * @return the enttry that matches the id being searched for
+         */
+
+         
+
+         /**
+          * This will return all entries with a given subject
+          * 
+          * @return a list of entries with the selected subject.
+          */
+
+
+        private function read($whereClause, $bindable) {
+            
             $query = "SELECT "
-                .   "s.subject as subject_description, "
-                .   "p.id, "
-                .   "p.subject, "
-                .   "p.dateOfEntry, "
-                .   "p.entry, "
-                .   "p.isdeleted "
-                . "FROM "
-                .   JOURNAL_TABLE . " p "
-                . "LEFT JOIN "
-                .    "subject s ON p.subject = s.id"
-                . " WHERE "
-                .   "p.isDeleted = false "
-                . "ORDER by "
-                .   "p.id DESC";
-                
+            .   "s.subject as subject_description, "
+            .   "p.id, "
+            .   "p.subject, "
+            .   "p.dateOfEntry, "
+            .   "p.entry, "
+            .   "p.isdeleted "
+            . "FROM "
+            .   JOURNAL_TABLE . " p "
+            . "LEFT JOIN "
+            .    "subject s ON p.subject = s.id"
+            . $whereClause
+            . "ORDER by "
+            .   "p.id DESC";
+            
             // echo $query;
 
             $stmt = $this->conn->prepare($query);
 
+            $stmt -> bindParam(1, $bindable);
+
             $stmt->execute();
 
             return $stmt;
+
         }
+
     
 }
 ?>
