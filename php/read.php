2b28a4911c16d38ed02d9a08152aedb4e8efa5dc
Sescheraun <Sescheraun@gmail.com>
2018-11-25 14:18:23 -0600

    Rewriting the code to decide between subject and journal

--- php/read.php
+++ php/read.php
@@ -8,25 +8,41 @@ header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,
 
 include_once("Database.php");
 include_once('Journal.php');
+include_once('Subject.php');
 
 
 $dBase = new Database();
 $db = $dBase->getConnection();
 
-//Call the read method bassed on what was asked for
-
 $journal = new Journal($db);
-if (isset($_GET['id'])){
+$subject = new Subject($db);
+
+$table = "";
+
+if(isset($_GET['table'])) {
+    $table = $_GET['table'] ;
+}
+ 
+if ($table == "subject") {
+    
+    $result = $subject->read();
+
+} else if (isset($_GET['id'])){
+    $table = "journal";
     //readByID not ready yet
     echo json_encode("{'data':{'result':'Read by ID not ready yet'}}");
     $result = "";
     die;
+
 } else if(isset($_GET['subject'])){
+    $table = "journal";
     //readBySubject not ready yet
     echo json_encode("{'data':{'result':'Read by subject not ready yet'}}");
     $result = "";
     die;
+
 } else {
+    $table = "journal";
     $result = $journal->readAll();
 }
 
@@ -35,23 +51,33 @@ if (isset($_GET['id'])){
 $rows = $result->rowCount();
 
 if ($rows > 0) {
-    $journalArray = array();
-    $journalArray['data'] = array();
+    $responseArray = array();
+    $responseArray['data'] = array();
 
-    while ($rows = $result->refch(PDO::FETCH_ASSOC)) {
-        exrtract($rows);
 
-        $journalEntry = array(
-            'id' => $id,
-            'date' => $dateOfEntry,
-            'subject' => $subject_description,
-            'entry' => html_entity_decode($entry)
-        );
+    
+    while ($rows = $result->fetch(PDO::FETCH_ASSOC)) {
+        extract($rows);
+        if ($table == "journal") {
+            $responseEntry = array(
+                'id' => $id,
+                'date' => $dateOfEntry,
+                'subject' => $subject_description,
+                'entry' => html_entity_decode($entry)
+            );
 
-        array_push($journalArray['data'], $journalEntry);
+        } else if ($table = "subject") {
+            $responseEntry = array(
+                'id' => $id,
+                'subject' => $subject
+            );
+            //  echo json_encode($responseEntry);
+        }
+
+        array_push($responseArray['data'], $responseEntry);
     }
 
-    echo json_encode($journalArray);
+    echo json_encode($responseArray);
 } else {
     echo json_encode("{'data':{'result':'There were no resutls'}}");
 }
