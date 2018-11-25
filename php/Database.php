430e611edcd97fe73b0c61f255f00ab1ead29af8
Sescheraun <Sescheraun@gmail.com>
2018-11-25 11:43:24 -0600

    Correcting several typos

--- php/Database.php
+++ php/Database.php
@@ -3,12 +3,13 @@
     class Database{
         private $conn;
 
-        public function connect() {
+        public function getConnection() {
             $this->conn = null;
 
             try {
                 $this->conn = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASSWORD );
-                $this-conn-setAttribute(PDO::ATTR_ERRMODE, PDO::ERR_ECEPTION);
+
+                $this -> conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch(PDOException $pdoException) {
                 echo "Connection to database failed"; 
             }
