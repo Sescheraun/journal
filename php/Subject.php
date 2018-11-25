ddbd530f78499f21753f343729b78a9c8d205063
Sescheraun <Sescheraun@gmail.com>
2018-11-25 14:20:37 -0600

    Creating the class to return a full list of possible subjects

---
+++ php/Subject.php
@@ -0,0 +1,59 @@
+<?php
+    class Subject {
+
+        private $conn;
+        private $id;
+        private $Subject;
+
+
+        public function __construct($db) {
+            $this->conn = $db;
+        }
+
+        public function __get($property) {
+            
+            if (property_exists($this, $property)) {
+                return $this->$property;
+            }
+        }
+
+        public function __set($property, $value) {
+            if ($property == '$conn') {
+                echo "There is an unhandled exception.";
+            } else if (property_exists($this, $property)) {
+                $this->$property = $value;
+            }
+        }
+        
+
+
+
+        /** 
+         * This is the read all method.
+         * 
+         * @return full array of the undeleted journal entries.
+         */
+        
+        public function read() {
+            
+            $query = "SELECT "
+            .   "id, "
+            .   "subject "
+            . "FROM "
+            .   SUBJECT_TABLE
+            . " ORDER by "
+            .   "subject ";
+            
+            // echo $query;
+
+            $stmt = $this->conn->prepare($query);
+
+            $stmt->execute();
+
+            return $stmt;
+
+        }
+
+    
+}
+?>
