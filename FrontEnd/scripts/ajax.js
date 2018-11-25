95f2bd4e908577c3075f454942565e34c3c1abe6
Sescheraun <Sescheraun@gmail.com>
2018-11-25 14:19:57 -0600

    adding code to get and build the list for the dropdown subject list

--- FrontEnd/scripts/ajax.js
+++ FrontEnd/scripts/ajax.js
@@ -9,8 +9,44 @@
 
             , type:"GET"
 
-            , data: "table=category"
+            , data: "table=subject"
             
+            , beforeSend: function() {
+                console.log("Sending ajax reguest");
+                console.log(crudURL);
+            }
+
+            , success:function(data) {
+                // console.log("Data recieved: " + data.data);
+                let topics = $('#subjects');
+                for(loop = 0; loop < data.data.length; loop++){
+
+                    let ID = (data.data[loop].id);
+                    let subject = $("<option>").text(data.data[loop].subject).attr("data-id", ID);
+
+                    console.log(subject.value);
+
+                    subject.appendTo(topics);
+                }
+            }
+
+            , error:function(xhr, status, error) {
+                console.log("ERROR: " + xhr.responseText);
+            }
+
+            , complete:function() {
+                console.log("Call complete");
+            }
+        })
+
+        // load all entries
+                
+        $.ajax(
+            {
+            url: crudURL
+
+            , type:"GET"
+           
             , beforeSend: function() {
                 console.log("Sending ajax reguest");
                 console.log(crudURL);
@@ -28,9 +64,6 @@
                 console.log("Call complete");
             }
         })
-
-        // load all entries
-       // $.AJAX
     })
 
     
