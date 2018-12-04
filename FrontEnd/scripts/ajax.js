85274c289186177c9609e3ab203b152a1dcc4908
Sescheraun <sescheraun@gmail.com>
2018-12-04 10:17:27 -0600

    Code debuged and working for for create post methods.  Started working on code to get and display all posts.

--- FrontEnd/scripts/ajax.js
+++ FrontEnd/scripts/ajax.js
@@ -2,6 +2,10 @@
     let readURL = "/journal/php/read.php";
     let postURL = "/journal/php/create.php";
     
+
+    /********************************************************************************
+    **                            Get all the subjects                             **
+    ********************************************************************************/
     $(document).ready( function() {
         // Load Categories
         $.ajax(
@@ -26,83 +30,106 @@
                 for(loop = 0; loop < reply.length; loop++){
                     
                     let category = reply[loop].category;
-                     let ID = reply[loop].id;
-                     console.log (ID);
-                     console.log(category);
-
+                    let ID = reply[loop].id;
+                    
                     let subject = $("<option>").text(reply[loop].category).attr("data-id", ID);
-
-                //     console.log(subject.value);
 
                     subject.appendTo(topics);
                 }
             }
-
             , error:function(xhr, status, error) {
                 console.log("ERROR: " + xhr.responseText);
             }
-
             , complete:function() {
                 console.log("Call complete");
             }
         })
 
+        /********************************************************************************
+        **                        Get all the journal entries                          **
+        ********************************************************************************/
+    $(document).ready( function() {
+        // Load Categories
+        $.ajax(
+            {
+            url: readURL
 
-        // Add a new post
+            , type:"GET"
+
+            , data: "table=subject"
+            
+            , beforeSend: function() {
+                console.log("Sending ajax reguest");
+                console.log(readURL);
+            }
+
+            , success:function(response) {
+                // console.log(response);
+                let reply = {};
+                reply = response.data;
+                let entries = {};
+                
+                for(loop = 0; loop < reply.length; loop++){
+                    
+                    let categorysubject = reply[loop].subject;
+                    let ID = reply[loop].id;
+                    
+                    let subject = $("<option>").text(reply[loop].category).attr("data-id", ID);
+
+                    subject.appendTo(topics);
+                }
+            }
+            , error:function(xhr, status, error) {
+                console.log("ERROR: " + xhr.responseText);
+            }
+            , complete:function() {
+                console.log("Call complete");
+            }
+        })
+
+        /********************************************************************************
+        **                              Post a new emtry                               **
+        ********************************************************************************/
         $("#addNewPost").on("click", function() {
             let selectedOption = $("#newPostTopic option:selected");
             let id = selectedOption.attr("data-id");
             let post = $.trim($("#newPostContent").val());
 
-            let postData = {"subject": id, "entry": post};
+            let postData = "subject=" + id + "&entry=" + post;
+            
             $.ajax({ 
                 url:postURL
-                , type: "POST"
+                , method: "POST"
                 , data: postData
-                , success: function(response) {
-                    let reply = {};
-                    reply = response;
-                    console.log(reply.data);
+                , dataType: "JSON"
+                , success: function(responseText) {
+                    let data = JSON.parse(responseText);
+                    console.log(data.data.result);
 
-                    for(loop = 0; loop < reply.length; loop++){
-                    
-                        let result = reply[loop].result;
-                        console.log(result);
-    
-    
-    
-            
-    
-                    }
+                    //todo: code needs to go here to put the confirmation on the page
+                    //and reset the form to empty.  Maybe a picture of a pony pointing
+                    //at the message.
+
+                }, error:function(xhr, status, error) {
+                    console.log("ERROR:");
+                    console.log(xhr.responseText);
+                    console.log(postData);
+                    console.log(status);
+                    console.log(error);
+
+                    //TODO: put some code here to put the failure on the webpage
                 }
+    
+                , complete:function() {
+                    console.log("Call complete");
+                }
+                
+                
+                
             })
         })
 
-        // // load all entries
-                
-        // $.ajax(
-        //     {
-        //     url: readURL
 
-        //     , type:"GET"
-           
-        //     , beforeSend: function() {
-        //         console.log("Sending ajax reguest");
-        //         console.log(readURL);
-        //     }
-
-        //     , success:function(data) {
-        //         console.log("Data recieved: " + data);
-        //     }
-
-        //     , error:function(xhr, status, error) {
-        //         console.log("ERROR: " + xhr.responseText);
-        //     }
-
-        //     , complete:function() {
-        //         console.log("Call complete");
-        //     }
-        // })
     })
 
     
