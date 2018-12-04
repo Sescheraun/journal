a80420d13feef412d8de0ad1f7b89c685491e214
Sescheraun <sescheraun@gmail.com>
2018-12-04 10:18:07 -0600

    create.php file now seems to be working fully

--- php/create.php
+++ php/create.php
@@ -1,22 +1,48 @@
 <?php 
-
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
-header('Access-Control-Allow-Methods: GET');
-header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
-
-
-include_once("Database.php");
-include_once('Journal.php');
-include_once('Subject.php');
+    /********************************************************************************
+    **                                   Header                                    **
+    **                                and Includes                                 **
+    ********************************************************************************/        
 
 
-$dBase = new Database();
-$db = $dBase->getConnection();
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Methods: POST');
+    header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
-$journal = new Journal($db);
-$subject = new Subject($db);
+    include_once("Database.php");
+    include_once('Journal.php');
+    include_once('Subject.php');
 
-echo json_encode("{'data':{'result':'the call worked'}}");
+    /********************************************************************************
+    **                       Initialize The Database connection                    **
+    ********************************************************************************/        
+    $dBase = new Database();
+    $db = $dBase->getConnection();
 
+    /********************************************************************************
+    **                          Initialize The table objects                       **
+    ********************************************************************************/ 
+    $journal = new Journal($db);
+    $subject = new Subject($db);
+
+    /********************************************************************************
+    **                       Get and add the data to the objects                   **
+    ********************************************************************************/ 
+
+    $subject = $_POST["subject"];
+    $entry = $_POST["entry"];
+
+    $journal->subject = $subject;
+    $journal->entry = $entry;
+
+    /********************************************************************************
+    **                     process the data into the Database                      **
+    ********************************************************************************/     
+
+    if ($journal->create()) {
+        echo json_encode('{"data":{"result":"Post was saved"}}');
+    } else {
+        echo json_encode('{"data":{"ERROR":"Post was not saved"}}');
+    }
 ?>
