20ee79a7bad96c2168d7863479ede5682c1fb8b1
Sescheraun <sescheraun@gmail.com>
2018-12-10 20:27:12 -0600

    Adding the code to handle setting and verifying a password and user name.

--- php/User.php
+++ php/User.php
@@ -21,43 +21,10 @@
         }
 
         /********************************************************************************
-        **                                 Verify User                                 **
-        ********************************************************************************/   
-        public function readUsers () {
-
-            $query = 'SELECT '
-                . 'userName, '
-                . 'password '
-                . 'FROM '
-                . 'user '
-                . 'WHERE '
-                . 'userName = :userName '
-                ;
-
-                $stmt = $this->conn->prepare($query);
-
-                $this->userName = htmlspecialchars(strip_tags($this->userName));
-
-                $stmt -> bindParam(':userName', $this->userName);
-    
-                $stmt->execute();
-
-                $result = $stmt->get_result();
-
-                echo $result;
-    
-                return confirmPassword($hashPassword);
-        }
-
-
-
-        /********************************************************************************
         **                                 Create User                                 **
         ********************************************************************************/
         
         public function createUser() {
-
-            echo $this->password;
 
             $query = 'INSERT INTO '
                 . 'users (userName, password) '
@@ -68,7 +35,6 @@
                 $stmt = $this->conn->prepare($query);
 
                 $this->userName = htmlspecialchars(strip_tags($this->userName));
-                $this->password = htmlspecialchars(strip_tags($this->password));
                 
                 $stmt -> bindParam(':userName', $this->userName);
                 
@@ -76,6 +42,8 @@
                 
                 $stmt->bindParam(':password', $password );
             
+                echo $this->muckedPassword($this->password) . "\n";
+
                 if ($stmt->execute()) {
                     return true;
                 } else {
@@ -85,19 +53,47 @@
 
 
         /********************************************************************************
+        **                                 Verify User                                 **
+        ********************************************************************************/   
+        public function readUsers() {
+            
+            $query = 'SELECT '
+                . 'userName, '
+                . 'password '
+                . 'FROM '
+                . 'users '
+                . 'WHERE '
+                . 'userName = :userName';
+
+                $stmt = $this->conn->prepare($query);
+
+                $this->userName = htmlspecialchars(strip_tags($this->userName));
+
+                $stmt -> bindParam(':userName', $this->userName);
+    
+                $stmt->execute();
+
+                $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+                return $this->confirmPassword($result[password]);
+
+
+        
+        }
+        /********************************************************************************
         **                               Hash Passwords                                **
         ********************************************************************************/
 
         private function muckedPassword($password){
-            return password_hash($password, PASSWORD_DEFAULT, ["cost" => 15]);
+            return password_hash($password, PASSWORD_DEFAULT);
         }
 
         /********************************************************************************
         **                              Confirm Password                               **
         ********************************************************************************/
 
-        private function confirmPassword(){
-
+        private function confirmPassword($hash) {
+            return password_verify($this->password, $hash);
         }
     }
 
